c19bb918c19fa436ed15dce0e92bb8c2f878b761
noobattech08 <bishalshood97@gmail.com>
2024-02-17 14:29:39 +0530

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .models import Category, Item
+
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'index.html', {
+        'categories': categories,
+        'items': items,
+    })
+
+
+def contact(request):
+    return render(request, 'contact.html')
